4b835b5baa045ee8f8ab73c30d7c3b63a6c8e94e
Jake Soenneker <jake@soenneker.com>
2026-03-10 13:17:50 -0500

    misc

--- test/Soenneker.GraphQL.Generator.Tests/GraphQLGeneratorTests.cs
+++ test/Soenneker.GraphQL.Generator.Tests/GraphQLGeneratorTests.cs
@@ -1,7 +1,3 @@
-using System.Net.Http;
-//using ConsoleApp1.Generated;
-//using ConsoleApp1.Generated;
-using Soenneker.GraphQL.Generator.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
 
@@ -10,17 +6,12 @@ namespace Soenneker.GraphQL.Generator.Tests;
 [Collection("Collection")]
 public sealed class GraphQLGeneratorTests : FixturedUnitTest
 {
-    private readonly IGraphQLGenerator _util;
-
     public GraphQLGeneratorTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
     {
-        _util = Resolve<IGraphQLGenerator>(true);
     }
 
     [Fact]
     public void Default()
     {
-      //  var client = new GraphQlClient(new GraphQlHttpClient(new HttpClient()));
-
     }
 }
